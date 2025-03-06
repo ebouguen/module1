2bf7fb400be1a963e147f8d45c9c4dd676d12be6
EricName <eric.bouguen@avanade.com>
2025-03-06 16:03:29 +0100

    Copilot Second Commit

---
+++ member.js
@@ -0,0 +1,11 @@
+function skillsMember() {
+    var member = {
+        name: "John",
+        age: 22,
+        skills: ["JavaScript", "React", "Node", "MongoDB"],
+        details: {
+            address: "USA"
+        }
+    }
+    return member;
+}
